8ed31f4ecdaaa8ce3e4b196eb7e66156d418038a
Aureliano Ventrella <aureliano.ventrella@eidosmedia.com>
2025-09-09 14:04:09 +0200

    New Social Card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -56,7 +56,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/neon-userguide-social-card.jpg',
     navbar: {
       title: 'Neon Docs',
       logo: {
